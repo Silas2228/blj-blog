3f5e2fd15267ab75eba4cb12108af49e65935308
Silas Reichlin <silasreichlin@bluewin.ch>
2020-11-25 08:37:46 +0100

    Database connection

--- Blog/homepage/homepage.php
+++ Blog/homepage/homepage.php
@@ -1,5 +1,41 @@
 <?php
-$name    = $_POST['name']    ?? '';
+
+$user = 'root';
+$password = '';
+$created_by = $_POST['name'] ?? '';
+$blog = $_POST['blog'] ?? '';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if(isset($_POST['name'])) {
+        $blog = $_POST['name'];
+    }
+}
+
+
+
+$pdo = new PDO('mysql:host=localhost;dbname=blog', $user, $password, [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+]);
+
+$stmt = $pdo->prepare('SELECT * FROM `datenblog` WHERE id = :id');
+$stmt->execute([':id' => 1]);
+
+foreach($stmt->fetchAll() as $x) {
+    var_dump($x);
+}
+
+
+$dbConnection = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
+$stmt = $dbConnection->prepare('INSERT INTO addresses (created_by, blog) 
+                                    VALUES (:created_by, :blog, now())');
+                    
+$stmt->execute([':blog' => $blog, 'created_by' => $created_by]);
+
+
+
+
 
 ?>
 
@@ -14,12 +50,15 @@ $name    = $_POST['name']    ?? '';
         include '..\nav\navigation.php';
     ?>
     <div class="gridhome">
-        <h2>Schreibe einen Blog:</h2>
+        <h2>Blog schreiben</h2>
+        <form action="homepage.php" method="POST">
         <label class="homelabels" for="name"><h2>Ihr Name:</h2></label>
         <input class="input" type="text" id="name" name="name" value="<?= $name ?? '' ?>">
+                
         <label class="homelabels" for="blog"><h2>Ihr Blog:</h2></label>
-        <input class="inputblog" type="text" id="blog" name="blog" value="<?= $blog?? '' ?>">
-      
+        <textarea class ="inputblog" type="text" id="blog" rows="6" cols="40" class="cssdesign" value="<?= $blog?? '' ?>"></textarea> 
+        <button class="buttonnav" type="submit" value="abschicken">Posten</button>
+        </form>
     </div>
 
 </body>
--- Blog/nav/navigation.php
+++ Blog/nav/navigation.php
@@ -3,7 +3,7 @@
     <h1 class="navtitel">Blog</h1>
     <ul class="flexbuttonsnav-container">
                 <li><a class="buttonnav" href="">Home</a></li>
-                <li><a class="buttonnav" href="">Bloggen</a></li>
+                <li><a class="buttonnav" href="../homepage/homepage.php">Bloggen</a></li>
                 <li><a class="buttonnav" href="">Blogs</a></li>
     </ul>
     
